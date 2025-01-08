cd676b4e2aa21e89bc10c8a163f98415516c6248
trung0501 <nguyentrungt151@gmail.com>
2025-01-08 15:23:26 +0700

    Add

--- Builder/Report.cs
+++ Builder/Report.cs
@@ -18,4 +18,20 @@ namespace BuilderDesignPattern
             Console.WriteLine("Footer :" + ReportFooter);
         }
     }
+    public abstract class ReportBuilder
+    {
+        protected Report reportObject;
+        public abstract void SetReportType();
+        public abstract void SetReportHeader();
+        public abstract void SetReportContent();
+        public abstract void SetReportFooter();
+        public void CreateNewReport()
+        {
+            reportObject = new Report();
+        }
+        public Report GetReport()
+        {
+            return reportObject;
+        }
+    }
 }
