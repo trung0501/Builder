9fb00d76bedb05810734f98e078d7cb42c13eaf5
trung0501 <nguyentrungt151@gmail.com>
2025-01-09 07:48:38 +0700

    Add

--- Builder/Report.cs
+++ Builder/Report.cs
@@ -34,4 +34,23 @@ namespace BuilderDesignPattern
             return reportObject;
         }
     }
+    class ExcelReport : ReportBuilder
+    {
+        public override void SetReportContent()
+        {
+            reportObject.ReportContent = "Excel Content Section";
+        }
+        public override void SetReportFooter()
+        {
+            reportObject.ReportFooter = "Excel Footer";
+        }
+        public override void SetReportHeader()
+        {
+            reportObject.ReportHeader = "Excel Header";
+        }
+        public override void SetReportType()
+        {
+            reportObject.ReportType = "Excel";
+        }
+    }
 }
