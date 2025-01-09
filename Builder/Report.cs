a0bd104513e838b86d96b998698a1bdf920d269f
trung0501 <nguyentrungt151@gmail.com>
2025-01-09 07:52:53 +0700

    Add

--- Builder/Report.cs
+++ Builder/Report.cs
@@ -53,4 +53,23 @@ namespace BuilderDesignPattern
             reportObject.ReportType = "Excel";
         }
     }
+    public class PDFReport : ReportBuilder
+    {
+        public override void SetReportContent()
+        {
+            reportObject.ReportContent = "PDF Content Section";
+        }
+        public override void SetReportFooter()
+        {
+            reportObject.ReportFooter = "PDF Footer";
+        }
+        public override void SetReportHeader()
+        {
+            reportObject.ReportHeader = "PDF Header";
+        }
+        public override void SetReportType()
+        {
+            reportObject.ReportType = "PDF";
+        }
+    }
 }
