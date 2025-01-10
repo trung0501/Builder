058fa36ae66d3a8ef62944f83d9717dffaff90b8
trung0501 <nguyentrungt151@gmail.com>
2025-01-10 14:45:47 +0700

    Add

--- Builder/Report.cs
+++ Builder/Report.cs
@@ -72,4 +72,17 @@ namespace BuilderDesignPattern
             reportObject.ReportType = "PDF";
         }
     }
+    public class ReportDirector
+    {
+        public Report MakeReport(ReportBuilder reportBuilder)
+        {
+            reportBuilder.CreateNewReport();
+            reportBuilder.SetReportType();
+            reportBuilder.SetReportHeader();
+            reportBuilder.SetReportContent();
+            reportBuilder.SetReportFooter();
+            return reportBuilder.GetReport();
+        }
+    }
+
 }
