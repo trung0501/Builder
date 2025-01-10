e00f6019457d2d87ec30c54c6cbd3ab9734fcd35
trung0501 <nguyentrungt151@gmail.com>
2025-01-10 14:48:35 +0700

    Add

--- Builder/Report.cs
+++ Builder/Report.cs
@@ -84,5 +84,19 @@ namespace BuilderDesignPattern
             return reportBuilder.GetReport();
         }
     }
-
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Report report;
+            ReportDirector reportDirector = new ReportDirector();
+            PDFReport pdfReport = new PDFReport();
+            report = reportDirector.MakeReport(pdfReport);
+            report.DisplayReport();
+            Console.WriteLine("-------------------");
+            ExcelReport excelReport = new ExcelReport();
+            report = reportDirector.MakeReport(excelReport);
+            report.DisplayReport();
+        }
+    }
 }
